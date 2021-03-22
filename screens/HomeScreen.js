65a3f40bc1241027059deeb85f1973937e0251f7
suzukisan22 <getty2017@gmail.com>
2021-03-22 16:54:29 +0900

    webviewで動的に記事を表示

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -31,7 +31,7 @@ export default HomeScreen = (props) => {
             imageUrl={item.urlToImage}
             title={item.title}
             author={item.author}
-            onPress={() => navigation.navigate('Article')}
+            onPress={() => navigation.navigate('Article', { article: item })}
           />
         )}
         keyExtractor={(item, index) => index.toString()}

1e06736dc97849743c805e9df862fa98dfeb46de
suzukisan22 <getty2017@gmail.com>
2021-03-29 00:15:47 +0900

    dev-toolをインストール

--- store/index.js
+++ store/index.js
@@ -1,11 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import userReducer from './reducers/user'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 // storeをtree構造で管理する
 const rootReducers = combineReducers({
   user: userReducer
 })
 
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, composeWithDevTools())
 
 export default store;

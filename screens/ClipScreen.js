9f442cfbf68c996e19c56a71417648f32c0d9691
suzukisan22 <getty2017@gmail.com>
2021-03-29 08:59:20 +0900

    clipタブを追加

---
+++ screens/ClipScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+})
+
+export default ClipScreen = ({ route }) => {
+  return (
+    <SafeAreaView style={styles.container} >
+      <Text>Clip Screen</Text>
+    </SafeAreaView>
+  )
+}
+

4eb5da637100b7c4e0896e5aa544f4b43fdca47a
suzukisan22 <getty2017@gmail.com>
2021-03-29 12:59:21 +0900

    iconを追加

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen'
 import ArticleScreen from '../screens/ArticleScreen'
 import ClipScreen from '../screens/ClipScreen'
+import { FontAwesome } from '@expo/vector-icons'
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -35,10 +36,23 @@ const ClipStack = () => {
   )
 }
 
+const screenOption = ({route}) => ({
+  tabBarIcon: ({color, size}) => {
+    let iconName;
+    if (route.name == "Home") {
+      iconName = 'home'
+    } else if (route.name == "Clip") {
+      iconName = "bookmark"
+    }
+
+    return <FontAwesome name={iconName} size={size} color={color} />
+  },
+})
+
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
         <Tab.Screen name="Home" component={HomeStack} />
         <Tab.Screen name="Clip" component={ClipStack} />
       </Tab.Navigator>
